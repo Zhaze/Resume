d4579c4d7ca13b7b33a601f69ed4f7de1f661473
Zhaze <ZacharyScottHazen@gmail.com>
2018-12-04 19:59:36 -0700

    added routes for initial pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ReferencesComponent } from './references/references.component';
+import { SkillsComponent } from './skills/skills.component';
+
+const routes: Routes = [
+  { path: '/about', component: AboutComponent},
+  { path: '/contact', component: ContactComponent},
+  { path: '/experience', component: ExperienceComponent},
+  { path: '/references', component: ReferencesComponent},
+  { path: '/skills', component: SkillsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
