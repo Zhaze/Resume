0b95de82d9414e700e6aec0c327c8bda4c78abf9
Zhaze <ZacharyScottHazen@gmail.com>
2018-12-05 02:16:51 -0700

    properly aligned items in the navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { SkillsComponent } from './skills/skills.component';
@@ -25,7 +24,7 @@ import { NavigationComponent } from './navigation/navigation.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbCollapseModule
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
