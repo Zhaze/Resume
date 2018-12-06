762e89436cdacb81d59ce5829431c9828aaaa3f8
Zac Hazen <ZacharyScottHazen@gmail.com>
2018-12-06 14:15:42 -0700

    added space to phone pipe for better formatting

--- src/app/phone-number.pipe.ts
+++ src/app/phone-number.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class PhoneNumberPipe implements PipeTransform {
 
   transform(value: string): string {
-      return "(" + value.slice(0, 3) + ")" + value.slice(3, 6) + "-" + value.slice(6);
+      return "(" + value.slice(0, 3) + ") " + value.slice(3, 6) + "-" + value.slice(6);
   }
 
 }
