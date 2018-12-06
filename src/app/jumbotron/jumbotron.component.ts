40e236fd445b53329b060aae6566054a5657de14
Zac Hazen <ZacharyScottHazen@gmail.com>
2018-12-06 00:46:29 -0700

    added jumbotron to top of screen, aded carousel to jumbotron, added config to carousel and carousel-images.ts to store images for future expansion or removal.

--- src/app/jumbotron/jumbotron.component.ts
+++ src/app/jumbotron/jumbotron.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+import { IMAGES } from 'src/carousel-images'
 
 @Component({
   selector: 'app-jumbotron',
@@ -6,8 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./jumbotron.component.scss']
 })
 export class JumbotronComponent implements OnInit {
-
-  constructor() { }
+  images = IMAGES;
+  constructor(config: NgbCarouselConfig) {
+    config.interval = 3000;
+    config.wrap = true;
+    config.keyboard = false;
+    config.pauseOnHover = false;
+    config.showNavigationArrows = false;
+    config.showNavigationIndicators = false;
+  }
 
   ngOnInit() {
   }
---
+++ src/carousel-images.ts
@@ -0,0 +1,5 @@
+export const IMAGES = [
+  { path: 'assets/img/carousel/1.jpg'},
+  { path: 'assets/img/carousel/2.jpg'},
+  { path: 'assets/img/carousel/3.jpg'}
+];
