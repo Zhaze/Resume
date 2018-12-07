b9861b54d6d8837014674a507f79934f8c31a217
Zac Hazen <ZacharyScottHazen@gmail.com>
2018-12-06 18:11:07 -0700

    Added accordian for experience page

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbAccordionConfig} from '@ng-bootstrap/ng-bootstrap';
+import { EXPERIENCE } from '../experience';
 
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
-  styleUrls: ['./experience.component.scss']
+  styleUrls: ['./experience.component.scss'],
+  providers: [NgbAccordionConfig]
 })
 export class ExperienceComponent implements OnInit {
-
-  constructor() { }
+  experience = EXPERIENCE;
+  constructor(config: NgbAccordionConfig) {
+    config.closeOthers = false;
+    config.type = 'dark';
+  }
 
   ngOnInit() {
   }
---
+++ src/app/experienceClass.ts
@@ -0,0 +1,9 @@
+export class Experience {
+  title: string;
+  company: string;
+  location: string;
+  startDate: string;
+  endDate: string;
+  comments: string;
+  description: string[];
+}
