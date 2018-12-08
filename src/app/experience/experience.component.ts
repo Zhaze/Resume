8765a341358fe283c5c47f90cb9f6da386cfaf2c
Zac Hazen <ZacharyScottHazen@gmail.com>
2018-12-08 02:37:18 -0700

    changed the page layout and added some logic for highlighting selected jobs

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { EXPERIENCE } from '../experience';
+import { Experience } from './experienceClass';
+import { EXPERIENCE } from './experience';
 
 @Component({
   selector: 'app-experience',
@@ -7,11 +8,17 @@ import { EXPERIENCE } from '../experience';
   styleUrls: ['./experience.component.scss']
 })
 export class ExperienceComponent implements OnInit {
-  experience = EXPERIENCE;
+  experience: Experience[] = EXPERIENCE;
+  shadow = new Array(this.experience.length);
+  toggleShadow(i): void {
+    this.shadow[i] ? this.shadow.splice(i, 1, false) : this.shadow[i] = !this.shadow[i];
+  };
+
   constructor() {
-  }
+
+  };
 
   ngOnInit() {
-  }
+  };
 
-}
+};
