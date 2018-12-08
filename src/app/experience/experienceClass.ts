1c2b8b822ef185791b7a7170d77992154244a3ba
Zac Hazen <ZacharyScottHazen@gmail.com>
2018-12-08 02:35:55 -0700

    moived data files into their respective folders

---
+++ src/app/experience/experienceClass.ts
@@ -0,0 +1,9 @@
+export class Experience {
+  title: string;
+  company: string;
+  location: string;
+  startDate: Date;
+  endDate: Date;
+  comments: string;
+  description: string[];
+}
---
+++ src/app/navigation/info.ts
@@ -0,0 +1,5 @@
+export const INFO: Object = {
+  name: "Zac Hazen",
+  email: "ZascharyScottHazen@gmail.com",
+  phone: "7204316293"
+}
