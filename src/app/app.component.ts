b1d98b010786219ecc7c3d3cf53ef375adb7a6de
Zac Hazen <ZacharyScottHazen@gmail.com>
2018-12-12 11:11:07 -0700

    left theme toggle on small screens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,8 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title: string = 'Resume';
-  theme: string = 'dark';
+  theme: string = 'light';
   toggleTheme(): void {
-    this.theme === 'dark' ? this.theme = 'light' : this.theme = 'dark';
+    this.theme === 'light' ? this.theme = 'dark' : this.theme = 'light';
   }
 }
