76416a4b7b18750a13e6dff0657fb1a61e28b71a
Zac Hazen <ZacharyScottHazen@gmail.com>
2018-12-22 00:10:58 -0700

    Theme changes affect dialogs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { OverlayContainer } from '@angular/cdk/overlay';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,7 +12,11 @@ export class AppComponent {
   title: string = 'Resume';
   theme: string = 'light';
   toggleTheme(x): void {
-    console.log(x.theme);
+    this.overlayContainer.getContainerElement().classList.remove(this.theme + '-theme');
     this.theme = x.theme;
+    this.overlayContainer.getContainerElement().classList.add(this.theme + '-theme');
+  }
+  constructor(public overlayContainer: OverlayContainer) {
+    overlayContainer.getContainerElement().classList.add(this.theme + '-theme');
   }
 }
